2c7f72cade67ab369b524378a1897559f99b3051
Rafael Marinho <rafael.mgf2001@gmail.com>
2021-08-14 11:21:47 -0300

    validaçao de operadores

--- main.js
+++ main.js
@@ -3,24 +3,31 @@ if (typeof kotlin === 'undefined') {
 }var main = function (_, Kotlin) {
   'use strict';
   var toString = Kotlin.toString;
+  var ensureNotNull = Kotlin.ensureNotNull;
+  var takeLast = Kotlin.kotlin.text.takeLast_6ic1pp$;
+  var equals = Kotlin.equals;
   var println = Kotlin.kotlin.io.println_s8jyv4$;
+  var throwCCE = Kotlin.throwCCE;
   function insert(num) {
-    var tmp$, tmp$_0;
+    var tmp$, tmp$_0, tmp$_1;
     var numero = (tmp$ = document.getElementById('output')) != null ? tmp$.innerHTML : null;
     (tmp$_0 = document.getElementById('output')) != null ? (tmp$_0.innerHTML = numero + toString(num)) : null;
+    if (equals(takeLast(ensureNotNull(numero), 2), '//') || equals(takeLast(numero, 3), '***') || equals(takeLast(numero, 2), '++')) {
+      disable();
+      var tmp$_2 = ensureNotNull(document.getElementById('output'));
+      var endIndex = numero.length - 1 | 0;
+      tmp$_2.innerHTML = numero.substring(0, endIndex);
+    } else if (equals(takeLast(numero, 2), '--')) {
+      disableMinus();
+      var tmp$_3 = ensureNotNull(document.getElementById('output'));
+      var endIndex_0 = numero.length - 1 | 0;
+      tmp$_3.innerHTML = numero.substring(0, endIndex_0);
+    } else {
+      enable();
+      enableMinus();
+      (tmp$_1 = document.getElementById('output')) != null ? (tmp$_1.innerHTML = numero + num) : null;
+    }
   }
-  function clear() {
-    var tmp$;
-    (tmp$ = document.getElementById('output')) != null ? (tmp$.innerHTML = '') : null;
-  }
-  function back() {
-    var tmp$, tmp$_0;
-    var output = (tmp$ = document.getElementById('output')) != null ? tmp$.innerHTML : null;
-    if (output != null) {
-      if ((tmp$_0 = document.getElementById('output')) != null) {
-        var endIndex = output.length - 1 | 0;
-        tmp$_0.innerHTML = output.substring(0, endIndex);
-      }}}
   function calculate() {
     var tmp$, tmp$_0;
     var result = (tmp$ = document.getElementById('output')) != null ? tmp$.innerHTML : null;
@@ -28,14 +35,60 @@ if (typeof kotlin === 'undefined') {
       try {
         (tmp$_0 = document.getElementById('output')) != null ? (tmp$_0.innerHTML = eval(result)) : null;
       } catch (err) {
-        clear();
+        enable();
+        enableMinus();
         println('Entrada invalida!');
       }
     }}
+  function back() {
+    var resultado = ensureNotNull(document.getElementById('output')).innerHTML;
+    var tmp$ = ensureNotNull(document.getElementById('output'));
+    var endIndex = resultado.length - 1 | 0;
+    tmp$.innerHTML = resultado.substring(0, endIndex);
+    enable();
+    enableMinus();
+  }
+  function clear() {
+    ensureNotNull(document.getElementById('output')).innerHTML = '';
+    enable();
+    enableMinus();
+  }
+  function disable() {
+    var tmp$, tmp$_0, tmp$_1;
+    var btn1 = Kotlin.isType(tmp$ = document.getElementById('btn1'), HTMLButtonElement) ? tmp$ : throwCCE();
+    btn1.disabled = true;
+    var btn2 = Kotlin.isType(tmp$_0 = document.getElementById('btn2'), HTMLButtonElement) ? tmp$_0 : throwCCE();
+    btn2.disabled = true;
+    var btn3 = Kotlin.isType(tmp$_1 = document.getElementById('btn3'), HTMLButtonElement) ? tmp$_1 : throwCCE();
+    btn3.disabled = true;
+  }
+  function enable() {
+    var tmp$, tmp$_0, tmp$_1;
+    var btn1 = Kotlin.isType(tmp$ = document.getElementById('btn1'), HTMLButtonElement) ? tmp$ : throwCCE();
+    btn1.disabled = false;
+    var btn2 = Kotlin.isType(tmp$_0 = document.getElementById('btn2'), HTMLButtonElement) ? tmp$_0 : throwCCE();
+    btn2.disabled = false;
+    var btn4 = Kotlin.isType(tmp$_1 = document.getElementById('btn4'), HTMLButtonElement) ? tmp$_1 : throwCCE();
+    btn4.disabled = false;
+  }
+  function disableMinus() {
+    var tmp$;
+    var btn3 = Kotlin.isType(tmp$ = document.getElementById('btn3'), HTMLButtonElement) ? tmp$ : throwCCE();
+    btn3.disabled = true;
+  }
+  function enableMinus() {
+    var tmp$;
+    var btn3 = Kotlin.isType(tmp$ = document.getElementById('btn3'), HTMLButtonElement) ? tmp$ : throwCCE();
+    btn3.disabled = false;
+  }
   _.insert = insert;
-  _.clear = clear;
-  _.back = back;
   _.calculate = calculate;
+  _.back = back;
+  _.clear = clear;
+  _.disable = disable;
+  _.enable = enable;
+  _.disableMinus = disableMinus;
+  _.enableMinus = enableMinus;
   Kotlin.defineModule('main', _);
   return _;
 }(typeof main === 'undefined' ? {} : main, kotlin);
--- main.kt
+++ main.kt
@@ -1,39 +1,85 @@
 import kotlinx.browser.*
-import org.w3c.dom.*  
+import org.w3c.dom.*
 
 @JsName("insert")
-fun insert(num:String?){
-  val numero = document.getElementById("output")?.innerHTML
-
-  document.getElementById("output")?.innerHTML = numero + num
-}
-
-@JsName("clear")
-fun clear(){
-  document.getElementById("output")?.innerHTML = ""
-}
-
-@JsName("back")
-fun back(){
-  val output = document.getElementById("output")?.innerHTML
-  if(output!=null){
-    document.getElementById("output")?.innerHTML = output.substring(0, output.length-1)
-  }
+fun insert(num: String?){
+  val numero = document.getElementById("output")?.innerHTML;
+  document.getElementById("output")?.innerHTML = numero + num;
   
+  if(numero!!.takeLast(2).equals("//") || numero.takeLast(3).equals("***") || numero.takeLast(2).equals("++")){
+    disable();
+    document.getElementById("output")!!.innerHTML = numero.substring(0, numero.length - 1)
+  }else if(numero.takeLast(2).equals("--")){
+    disableMinus();
+      document.getElementById("output")!!.innerHTML = numero.substring(0, numero.length - 1)
+  } else{
+    enable();
+    enableMinus();
+    document.getElementById("output")?.innerHTML = numero + num;
+    }
 }
 
 @JsName("calculate")
 fun calculate(){
-    val result = document.getElementById("output")?.innerHTML
-    if(result != null){
-      try{
-        document.getElementById("output")?.innerHTML = eval(result)
-      }
-      catch(err: dynamic){
-          clear()
-          println("Entrada invalida!")
-      }
+  val result = document.getElementById("output")?.innerHTML;
+  if(result != null){
+    try{
+      document.getElementById("output")?.innerHTML = eval(result)
     }
+    catch(err: dynamic){
+      enable();
+      enableMinus();
+      println("Entrada invalida!")
+    }
+  }
+}
+
+@JsName("back")
+fun back(){
+  val resultado = document.getElementById("output")!!.innerHTML;
+  document.getElementById("output")!!.innerHTML = resultado.substring(0, resultado.length - 1)
+  enable();
+  enableMinus();
+}
+
+@JsName("clear")
+fun clear(){
+  document.getElementById("output")!!.innerHTML = "";
+  enable();
+  enableMinus();
+}
+
+fun disable(){
+  val btn1 = document.getElementById("btn1") as HTMLButtonElement
+  btn1.disabled = true;
+   
+  val btn2 = document.getElementById("btn2") as HTMLButtonElement
+  btn2.disabled = true;
+   
+  val btn3 = document.getElementById("btn3") as HTMLButtonElement
+  btn3.disabled = true;
 
 }
 
+fun enable(){
+  val btn1 = document.getElementById("btn1") as HTMLButtonElement
+  btn1.disabled = false;
+
+  val btn2 = document.getElementById("btn2") as HTMLButtonElement
+  btn2.disabled = false;
+
+  val btn4 = document.getElementById("btn4") as HTMLButtonElement
+  btn4.disabled = false;
+}
+
+fun disableMinus(){
+  val btn3 = document.getElementById("btn3") as HTMLButtonElement
+
+  btn3.disabled = true;
+}
+
+fun enableMinus(){
+  val btn3 = document.getElementById("btn3") as HTMLButtonElement
+
+  btn3.disabled = false;
+}
